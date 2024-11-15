5de7ca1034a0c4dbdda0761d1cf5e052d1ddec5b
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-11-15 18:20:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import numpy as np
 
 from model import KeyPointClassifier, PointHistoryClassifier
 from utils import CvFpsCalc
+
+#Reference: pranayrishi
+
 def get_arguments():
     argument_parser = argparse.ArgumentParser()
 
